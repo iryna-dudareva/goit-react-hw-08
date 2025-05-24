4db3ea8038a1a1f6be00e4a11bd979063c459915
iren <dudareva.iryna.study@gmail.com>
2025-05-24 15:56:31 +0200

    add missing coomponents, refine App

--- src/App.jsx
+++ src/App.jsx
@@ -3,61 +3,65 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 
-import AppBar from './components/AppBar/AppBar'
-import Navigation from './components/Navigation/Navigation'
-import AuthNav from './components/AuthNav/AuthNav'
-import UserMenu from './components/UserMenu/UserMenu'
-
 import HomePage from './pages/HomePage/HomePage'
 import RegistrationPage from './pages/RegistrationPage/RegistrationPage'
 import LoginPage from './pages/LoginPage/LoginPage'
 import ContactsPage from './pages/ContactsPage/ContactsPage'
 
+import Layout from './components/Layout/Layout'
+import PrivateRoute from './components/PrivateRoute/PrivateRoute'
+import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute'
+
 import { refreshUser } from './redux/auth/operations'
-import { selectIsLoggedIn, selectIsRefreshing } from './redux/auth/selectors'
+import { selectIsRefreshing } from './redux/auth/selectors'
 
 const App = () => {
   const dispatch = useDispatch();
-  const isLoggedIn = useSelector(selectIsLoggedIn);
   const isRefreshing = useSelector(selectIsRefreshing);
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  if (isRefreshing) {
-    return <p>Loading...</p>;
-  }
+  if (isRefreshing) return <p>Loading...</p>;
 
   return (
     <>
-    <div className="container">
-      <AppBar />
 
-      <main className="main">
+
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route
-            path="/register"
-            element={
-              !isLoggedIn ? <RegistrationPage /> : <Navigate to="/contacts" />
-            }
-          />
-          <Route
-            path="/login"
-            element={!isLoggedIn ? <LoginPage /> : <Navigate to="/contacts" />}
-          />
-          <Route
-            path="/contacts"
-            element={isLoggedIn ? <ContactsPage /> : <Navigate to="/login" />}
-          />
+
+        <Route path="/" element={<Layout />} >
+
+        <Route index element={<HomePage />} />
+            <Route
+              path="/register"
+              element={
+                <RestrictedRoute>
+                  <RegistrationPage />
+                </RestrictedRoute>
+              } />
+            <Route
+              path="/login"
+              element={
+                <RestrictedRoute>
+                  <LoginPage />
+                </RestrictedRoute>
+              } />
+            <Route
+              path="/contacts"
+              element={
+                <PrivateRoute>
+                  <ContactsPage />
+                </PrivateRoute>
+              } />
           <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </main>
-    </div>
+          </ Route>
+          </Routes>
+          
     </>
   );
 };
 
-export default App
+export default App;
 
